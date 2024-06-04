c4a5551561b3f4cd1e73b37fe5dc751ec1836ae0
JLpensador <joaoluizborgesmorais@gmail.com>
2024-06-03 22:11:00 -0300

    Fixed: Estrutura consertada e atualizada #8

--- src/js/estoque.js
+++ src/js/estoque.js
@@ -1,34 +1,30 @@
-// productActions.js
-
 function alternarProduto(id) {
-    const estadoAtual = localStorage.getItem("produto_" + id);
-    if (estadoAtual === "pegado") {
-      localStorage.removeItem("produto_" + id);
-      return "Pegar";
-    } else {
-      localStorage.setItem("produto_" + id, "pegado");
-      return "Devolver";
-    }
+  const estadoAtual = localStorage.getItem("produto_" + id);
+  if (estadoAtual === "pegado") {
+    localStorage.removeItem("produto_" + id);
+    return "Pegar";
+  } else {
+    localStorage.setItem("produto_" + id, "pegado");
+    return "Devolver";
   }
-  
-  function atualizarBotão(id) {
-    const Botão = document.getElementById("Botão_" + id);
-    const estadoAtual = localStorage.getItem("produto_" + id);
-    if (estadoAtual === "pegado") {
-      Botão.textContent = "Devolver";
-    } else {
-      Botão.textContent = "Pegar";
-    }
+}
+
+function atualizarBotão(id) {
+  const Botão = document.getElementById("Botão_" + id);
+  const estadoAtual = localStorage.getItem("produto_" + id);
+  if (estadoAtual === "pegado") {
+    Botão.textContent = "Devolver";
+  } else {
+    Botão.textContent = "Pegar";
   }
-  
-  function cliqueBotão(id) {
-    const Botão = document.getElementById("Botão_" + id);
-    Botão.addEventListener("click", function () {
-      alternarProduto(id);
-      atualizarBotão(id);
-    });
-  }
-  
-  // Exportar as funções para serem usadas em outros arquivos
-  export { alternarProduto, atualizarBotão, cliqueBotão };
-  
+}
+
+function cliqueBotão(id) {
+  const Botão = document.getElementById("Botão_" + id);
+  Botão.addEventListener("click", function () {
+    alternarProduto(id);
+    atualizarBotão(id);
+  });
+}
+
+export { alternarProduto, atualizarBotão, cliqueBotão };
